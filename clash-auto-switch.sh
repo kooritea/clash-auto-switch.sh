ef3da882bceb81679418c46a6c5c5e292f82b48c
kooritea <ochatokori@gmail.com>
2020-08-22 15:00:30 +0800

    不再分为优先和次级，env中填写的顺序依次往下寻找

--- clash-auto-switch.sh
+++ clash-auto-switch.sh
@@ -8,10 +8,6 @@ lock(){
 unlock(){
 	rm $lockfilepath
 }
-if [ -e $lockfilepath ];then
-	exit 0
-fi
-lock
 urlencode() {
 	echo `jq -nr --arg v "$1" '$v|@uri'`
 }
@@ -29,10 +25,6 @@ gcurl(){
 setProxy(){
 	info "set proxy: $1 [$2ms]"
 	curl --noproxy "*" -s -H "Authorization: Bearer $token" -X PUT  "$api/proxies/$selectorName" -d "{\"name\":\"$1\"}"
-	if [ -e $recfilepath ];then
-		rm $recfilepath
-		whenRecovery "$1"
-	fi
 }
 getNowProxy(){
 	echo `echo $1 | jq -r ".$selectorName.now"`
@@ -47,15 +39,15 @@ match(){
 	done
 	echo $result
 }
-isFirst(){
-	local has
-	for item in ${firstProxy[*]};do
-		has=`echo $1 | grep -c "$item"`
-		if [ "$has" -ne 1 ];then
-			return 1
-		fi	
-	done
-	return 0
+testProxyName(){
+	#$2.test($1)
+	local has=`echo "$1" | grep -c "$2"`
+	if [ "$has" -ne 0 ];then
+		#匹配失败
+		return 0
+	else
+		return 1
+	fi
 }
 findProxyAndSet(){
 	local minDelay=9999
@@ -82,50 +74,59 @@ findProxyAndSet(){
 	fi
 }
 
+if [ -e $lockfilepath ];then
+	exit 0
+fi
+lock
 
 proxies=`gcurl /proxies | jq -r ".proxies"`
 nowProxy=`getNowProxy "$proxies"`
 nowdelay=`ping "$nowProxy"`
-isFirst "$nowProxy"
+testProxyName "$nowProxy" ${selectProxyRule[0]}
+
 if [ 0 -eq $? ];then
 	if [ "null" != "$nowdelay" ];then
 		unlock
 		exit 0
 	else
-		info "当前代理不可用"
+		info "当前代理[$nowProxy]不可用"
 	fi
 else
-	info "当前为非[${firstProxy[*]}]代理"
+	info "当前为非[${selectProxyRule[0]}]代理: [$nowProxy][$nowdelay]"
 fi
 
-fProxy=`echo $proxies | jq -r ".$selectorName.all"`
-fProxy=`match "${firstProxy[*]}" "$fProxy"`
+Proxys=`echo $proxies | jq -r ".$selectorName.all"`
 
-findProxyAndSet "$fProxy"
-if [ 0 -ne "$?" ];then
-	info "无可用[${firstProxy[*]}]线路"
-	
-	isFirst "$nowProxy"
-	if [ 0 -ne $? ];then
-		# 当前已经是次选代理
+for item in ${selectProxyRule[*]};do
+
+	testProxyName "$nowProxy" "$item"
+	if [ 0 -eq $? ];then
+		nowdelay=`ping "$nowProxy"`
 		if [ "null" != "$nowdelay" ];then
-			unlock
-			exit 0
+		# 当前已是次级代理且可用
+		info "当前已是次级代理[$item]且可用: [$nowProxy][$nowdelay]"
+		unlock
+		exit 0
 		fi
 	fi
-
-	#搜索次选代理
-	sProxy=`echo $proxies | jq -r ".$selectorName.all"`
-	sProxy=`match "${secondProxy[*]}" "$sProxy"`
-	findProxyAndSet "$sProxy"
+	matchProxys=`match "$item" "$Proxys"`
+	echo "$matchProxys"
+	findProxyAndSet "$matchProxys"
 	if [ 0 -eq "$?" ];then
-		newProxies=`gcurl /proxies | jq -r ".proxies"`
-		newNow=`getNowProxy "$newProxies"`
-		info "无可用[${firstProxy[*]}]线路,切换到：$newNow"
+		if [ -e $recfilepath ];then
+			rm $recfilepath
+			whenRecovery "$1"
+		fi
+		unlock
+		exit 0
 	else
-		info "无可用[${firstProxy[*]}]线路和[${secondProxy[*]}]线路"
-		echo "" > $recfilepath
-		whenNotProxy
+		info "无可用[$item]线路"
 	fi
+done
+
+if [ ! -e $recfilepath ];then
+	echo "" > $recfilepath
+	whenNotProxy
 fi
 unlock
+exit 0
